07f29e0c1d765f1c111e5ffeba55af6e13f6feb1
saumya <2saumyaray@gmail.com>
2013-10-12 16:56:38 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,82 @@
+//Use Express framework for nodeJS
+var express = require("express");
+
+//use mongoDB
+var mongo = require('mongodb');
+
+//var app = express.createServer(express.logger());//deprecated
+var app = express();
+
+//middle-ware additions
+app.use(express.logger());
+app.use(express.static('./public'));//makes public folder as a static folder
+//app.use(express.static('./files'));//may be more if required
+
+
+app.set('view engine','jade');//sets the view engine
+app.set('views','./views');//sets path to the view files
+
+app.get('/', function(request, response){
+	//response.send('<h1>Home</h1>');
+	//jade
+	response.render('index');
+
+	/*
+	response.render('index', {
+		title: 'Home'
+	});
+	*/
+});
+
+app.get('/say-hello', function(request, response){
+  response.render('hello');
+});
+
+app.get('/about', function(request, response){
+  response.send('About');
+});
+
+app.get('/contact', function(request, response){
+  response.send('Contact');
+});
+
+app.get('/mongo', function(request, response){
+	//mongoDB test
+	/*
+	var mongoUri = 'mongodb://localhost/mydb';
+	mongo.Db.connect(mongoUri, function (err, db) {
+	db.collection('mydocs', function(er, collection) {
+	    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+	    });
+	  });
+	});
+	*/
+	/*
+	var connect = function(callback){
+		mongoose.connect('mongodb://xxx:xxx@ds037647.mongolab.com:37647/kitties-api',callback);
+	}
+	*/
+	var mongoUri = 'mongodb://saumyaDB:saumyaDBPW@ds049598.mongolab.com:49598/saumya-ray';
+	mongo.Db.connect(mongoUri, function (err, db) {
+		//response.send('Connected to DB : mongodb : driver');
+		if(err){
+			response.send(err);
+		}else{
+			response.send('Connected to DB : mongodb : driver');
+		}
+	});
+	/*
+	mongoose.connect(mongoUri,function(){
+		response.send('Connected to DB : mongoose : driver');
+	});
+	*/
+});
+
+
+
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
