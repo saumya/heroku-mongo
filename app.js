d6c2199525a8ef3b9a685bd7578d28ff5f1f49df
saumya <2saumyaray@gmail.com>
2013-10-13 15:42:18 +0530

    inserted data

--- app.js
+++ app.js
@@ -64,16 +64,19 @@ app.get('/mongo', function(request, response){
 		}else{
 			//response.send('Connected to DB : mongodb : driver');
 			//now work with DB Server
-			/*
+			
 			var collection = db.collection('test');//get the DB selected
 			//put some data
 			var doc1 = {'hello':'doc1'};
   			var doc2 = {'hello':'doc2'};
-  			collection.insert(doc1);	
-  			collection.insert(doc2);
-  			*/
-  			db.unicorns.insert({name: 'Aurora', gender: 'f', weight: 450});
-  			response.send('Data is put on the DB !!');
+  			//collection.insert(doc1);	
+  			collection.insert(doc1,doc2,function(err,result){
+  				if(err){
+  					response.send(err);
+  				}else{
+  					response.send('Data is put on the DB !!');
+  				}
+  			});
 		}
 	});
 	/*
